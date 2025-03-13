6f032a7193c888d7ed72bba2e7f5ccfadd3041b9
YangCheng <Yang.Cheng@ibm.com>
2025-03-13 15:18:13 +0900

    20250313

---
+++ 【NBS】現行NBS抽出データ/30_work/3_データ抽出リハ(事前移行)/sql/2_4_technotes.sql
@@ -0,0 +1,21 @@
+SELECT
+    t.tan_id AS '解決策番号'
+    , t.short_desc AS 'タイトル'
+    , ISNULL(t.edi_sub_title,'') AS 'サブタイトル'
+    , ISNULL(format(t.creation_ts, 'yyyyMMdd'),'') AS '作成日'
+    , ISNULL(format(t.edi_check_date, 'yyyyMMdd'),'') AS '更新年月日'
+    , ISNULL(u1.lastname + ' ' + u1.firstname,'') AS '登録者'
+    , ISNULL(t.long_desc,'') AS '解決策内容'
+    , c1.value as 'カテゴリ1'
+    , ISNULL(c2.value,'') as 'カテゴリ2'
+    , ISNULL(u2.lastname + ' ' + u2.firstname,'') AS '作成者'
+    , ISNULL(u3.lastname + ' ' + u3.firstname,'') AS '更新者'
+    , ISNULL(format(t.delta_ts, 'yyyyMMdd'),'') AS '更新日' 
+FROM
+    technotes t 
+    LEFT OUTER JOIN users u1 ON t.assigned_to = u1.userid 
+    LEFT OUTER JOIN users u2 ON t.created_by = u2.userid 
+    LEFT OUTER JOIN users u3 ON t.delta_sy_user = u3.userid 
+    LEFT OUTER JOIN edi71_category_1 c1 ON c1.op_synonym = t.edi_category_1 
+    LEFT OUTER JOIN edi72_category_2 c2 ON c2.op_synonym = t.edi_category_2
+order by t.creation_ts;
